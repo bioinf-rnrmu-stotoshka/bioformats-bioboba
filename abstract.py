777e3b5d8799b0bf9ff5f0862c581718c13b387b
Seitsan <den.kolodin.06@gmail.com>
2025-10-17 22:03:24 +0300

    UPD Reader, SequenceReader, +GenomicDataReader

--- abstract.py
+++ abstract.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Iterator, List, Dict, Tuple
+from typing import Iterator, List, Dict
 from record import Record, SequenceRecord
 from pathlib import Path
 
